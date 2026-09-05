d8547b8b75a9ea41f78e98777e9f639ac618726e
dang160898 <danglaw20@gmail.com>
2026-09-05 18:55:56 +0700

    Update storage.js

--- api/storage.js
+++ api/storage.js
@@ -3,6 +3,7 @@ import { Redis } from '@upstash/redis';
 const redis = new Redis({
   url: process.env.UPSTASH_REDIS_REST_KV_REST_API_URL,
   token: process.env.UPSTASH_REDIS_REST_KV_REST_API_TOKEN,
+  automaticDeserialization: false,
 });
 
 export default async function handler(req, res) {
